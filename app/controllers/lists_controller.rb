d63efa27d180a0332fd5b4c8c16287893b732940
Edgar Ortega Ramírez <EdgarOrtegaRamirez@outlook.com>
2012-12-11 11:13:47 -0500

    Added create lists and tasks by AJAX, deleted unnecessary views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,29 @@
 class ListsController < ApplicationController
   
   def index
+    @list = List.new
     @lists = List.all
   end
 
-  def new
-  end
-
   def create
+    @list = List.new(params[:list])
+    @list.user = current_user
+    
+    if @list.save
+      respond_to do |format|
+        format.js
+      end
+    else
+
+    end
   end
 
   def show
+    @list = List.find(params[:id])
+    @tasks = Task.where(list_id: @list.id)
+    respond_to do |format|
+      format.js
+    end
   end
 
   def edit
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
-  def index
-  end
-
-  def new
-  end
 
   def create
+    @list = List.find(params[:list_id])
+    @task = Task.new(params[:task])
+    @task.list_id = params[:list_id]
+
+    @task.save
+    respond_to do |format|
+      format.js
+    end
   end
 end
