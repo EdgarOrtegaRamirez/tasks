4da92039214331d60a58c99beb272a77a8ed5d1d
Edgar Ortega Ramírez <EdgarOrtegaRamirez@outlook.com>
2012-12-14 10:34:48 -0500

    Deleted simple_form custom wrapper and added css style for hover tasks
    Added css style when hover tasks list.
    Removed simple_form custom wrapper that isn't used anymore

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -139,14 +139,4 @@ SimpleForm.setup do |config|
 
   # Cache SimpleForm inputs discovery
   # config.cache_discovery = !Rails.env.development?
-
-  config.wrappers :inline_checkbox, tag: 'span', class: 'control-group', error_class: 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.wrapper tag: 'span', class: 'controls' do |ba|
-      ba.use :label_input, wrap_with: { class: 'inline' }
-      ba.use :error, wrap_with: { tag:'span', :class => 'help-inline' }
-      ba.use :hint,  wrap_with: { tag: 'p', :class => 'help-block' }
-    end
-  end
 end
